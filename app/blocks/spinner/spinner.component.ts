8ab5c81e1acaee34cfd23bc67deeca3399975c3e
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2016-04-01 11:48:17 +0100

    format code

--- app/blocks/spinner/spinner.component.ts
+++ app/blocks/spinner/spinner.component.ts
@@ -6,29 +6,29 @@ import { ISpinnerState, SpinnerService } from './spinner.service';
 const ACTIVE_CLASS = 'is-active';
 
 @Component({
-  selector: 'story-spinner',
-  template: `
+    selector: 'story-spinner',
+    template: `
     <div
       class="spinner mdl-spinner mdl-js-spinner mdl-spinner--single-color"
       [class.is-active]="visible"></div>
   `,
-  styles: [`.spinner {position: absolute;left: 46%;top: 12%`]
+    styles: [`.spinner {position: absolute;left: 46%;top: 12%`]
 })
 
 export class SpinnerComponent implements OnDestroy, OnInit {
-  visible = false;
+    visible = false;
 
-  private _spinnerStateChanged: Subscription;
+    private _spinnerStateChanged: Subscription;
 
-  constructor(private _spinnerService: SpinnerService) { }
+    constructor(private _spinnerService: SpinnerService) { }
 
-  ngOnInit() {
-    componentHandler.upgradeDom();
-    this._spinnerStateChanged = this._spinnerService.spinnerState
-      .subscribe((state: ISpinnerState) => this.visible = state.show);
-  }
+    ngOnInit() {
+        componentHandler.upgradeDom();
+        this._spinnerStateChanged = this._spinnerService.spinnerState
+            .subscribe((state: ISpinnerState) => this.visible = state.show);
+    }
 
-  ngOnDestroy() {
-    this._spinnerStateChanged.unsubscribe();
-  }
+    ngOnDestroy() {
+        this._spinnerStateChanged.unsubscribe();
+    }
 }
